177e01a3f02594e43b80fa63a96eaef2257bfa1c
Marcello Duarte <marcello.duarte@gmail.com>
2011-09-06 21:34:53 +0100

    Refactoring further the score method

--- lib/BowlingGame.php
+++ lib/BowlingGame.php
@@ -15,16 +15,15 @@ class BowlingGame {
 
         for ($frame = 1; $frame <= 10; $frame++) {
             if ($this->isStrike($frameIndex)) {
-                $score += 10 + $this->rolls[$frameIndex + 1] +
-                               $this->rolls[$frameIndex + 2];
+                $score += 10 + $this->strikeBonus($frameIndex);
                 $frameIndex++;
-                continue;
             } elseif ($this->isSpare($frameIndex)) {
-                $score += 10 + $this->rolls[$frameIndex + 2];
+                $score += 10 + $this->spareBonus($frameIndex);
+                $frameIndex += 2;
             } else {
-                $score += $this->rolls[$frameIndex] + $this->rolls[$frameIndex + 1];
+                $score += $this->sumOfBallsInFrame($frameIndex);
+                $frameIndex += 2;
             }
-            $frameIndex += 2;
         }
 
         return $score;
@@ -40,5 +39,18 @@ class BowlingGame {
         return $this->rolls[$frameIndex] + $this->rolls[$frameIndex + 1] == 10;
     }
     
+    private function strikeBonus($frameIndex)
+    {
+        return $this->rolls[$frameIndex + 1] + $this->rolls[$frameIndex + 2];
+    }
     
+    private function spareBonus($frameIndex)
+    {
+        return $this->rolls[$frameIndex + 2];
+    }
+    
+    private function sumOfBallsInFrame($frameIndex)
+    {
+        return $this->rolls[$frameIndex] + $this->rolls[$frameIndex + 1];
+    }
 }
